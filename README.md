ebe2b61ec0785674ae9b091bd1a441e276751b8d
Marvin <marvinraj77@gmail.com>
2025-08-30 14:14:55 +0800

    added folder structure

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-marvin's portfolio
+# marvin's portfolio
+
+### pages
+1. home
+2. about 
+3. projects
+4. experience
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,16 @@
+import Navbar from "../components/Navbar"
+
+function HomePage() {
+  return (
+    <div>
+        <div><Navbar /></div>
+        <main>
+            <section>
+                <h1 className="text-3xl font-extrabold">HomePage</h1>
+            </section>
+        </main>
+    </div>
+  )
+}
+
+export default HomePage
